41a0dda4a551180c9d14dfb32ce9b4c69f47f81c
the-debug-arena <thedebugarena@gmail.com>
2024-03-18 22:09:27 +0530

    Search Filter

--- src/components/adminHome.js
+++ src/components/adminHome.js
@@ -1,28 +1,19 @@
-import React, { Component, useEffect, useState } from "react";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useState } from "react";
+import { faTrash,faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import ReactPaginate from 'react-paginate';
-import { useRef } from "react";
-export default function AdminHome({ userData }) {
 
+export default function AdminHome({}) {
   //setting state
   const [data, setData] = useState([]);
-  const [limit,setLimit]=useState(5);
-  const [pageCount,setPageCount]=useState(1);
-  const currentPage=useRef();
-
-
+  const [searchQuery,setSearchQuery]=useState("")
 
   useEffect(() => {
-    currentPage.current=1;
-    // getAllUser();
-    getPaginatedUsers();
-  }, []);
-
+    getAllUser();
+  },[searchQuery]);
 
   //fetching all user
   const getAllUser = () => {
-    fetch("http://localhost:5000/getAllUser", {
+    fetch(`http://localhost:5000/getAllUser?search=${searchQuery}`, {
       method: "GET",
     })
       .then((res) => res.json())
@@ -32,14 +23,11 @@ export default function AdminHome({ userData }) {
       });
   };
 
-
-
-//logout
+  //logout
   const logOut = () => {
     window.localStorage.clear();
     window.location.href = "./sign-in";
   };
-
 
   //deleting user
   const deleteUser = (id, name) => {
@@ -64,41 +52,40 @@ export default function AdminHome({ userData }) {
     } else {
     }
   };
-
-  //pagination
-  function handlePageClick(e) {
-    console.log(e);
-   currentPage.current=e.selected+1;
-    getPaginatedUsers();
-   
-
-  }
-  function changeLimit(){
-    currentPage.current=1;
-    getPaginatedUsers();
-  }
-
-  function getPaginatedUsers(){
-    fetch(`http://localhost:5000/paginatedUsers?page=${currentPage.current}&limit=${limit}`, {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data, "userData");
-        setPageCount(data.pageCount);
-        setData(data.result)
-        
-       
-      });
+  function handleSearch(e){
+    setSearchQuery(e.target.value)
 
   }
 
   return (
-    <div className="auth-wrapper" style={{ height: "auto" }}>
-      <div className="auth-inner" style={{ width: "auto" }}>
-        <h3>Welcom Admin</h3>
-        <table style={{ width: 500 }}>
-          <tr>
+    <div className="auth-wrapper" style={{ height: "auto", marginTop: 50 }}>
+      <div className="auth-inner" style={{ width: "fit-content" }}>
+        <h3>Welcome Admin</h3>
+        <div style={{ position: "relative", marginBottom: 10 }}>
+          <FontAwesomeIcon
+            icon={faSearch}
+            style={{ position: "absolute", left: 10, top: 13, color: "black" }}
+          />
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={handleSearch}
+            style={{
+              padding: "8px 32px 8px 32px",
+              borderRadius: "4px",
+              border: "1px solid #ccc",
+              width: "100%",
+              boxSizing: "border-box",
+            }}
+          />
+          <span
+            style={{ position: "absolute", right: 10, top: 8, color: "#aaa" }}
+          >
+           {searchQuery.length>0?`Records Found ${data.length}`:`Total Records ${data.length}`} 
+          </span>
+        </div>
+        <table style={{ width: 700 }}>
+          <tr style={{ textAlign: "center" }}>
             <th>Name</th>
             <th>Email</th>
             <th>User Type</th>
@@ -106,7 +93,7 @@ export default function AdminHome({ userData }) {
           </tr>
           {data.map((i) => {
             return (
-              <tr>
+              <tr style={{ textAlign: "center" }}>
                 <td>{i.fname}</td>
                 <td>{i.email}</td>
                 <td>{i.userType}</td>
@@ -120,28 +107,12 @@ export default function AdminHome({ userData }) {
             );
           })}
         </table>
-        <ReactPaginate
-          breakLabel="..."
-          nextLabel="next >"
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={5}
-          pageCount={pageCount}
-          previousLabel="< previous"
-          renderOnZeroPageCount={null}
-          marginPagesDisplayed={2}
-          containerClassName="pagination justify-content-center"
-          pageClassName="page-item"
-          pageLinkClassName="page-link"
-          previousClassName="page-item"
-          previousLinkClassName="page-link"
-          nextClassName="page-item"
-          nextLinkClassName="page-link"
-          activeClassName="active"
-          forcePage={currentPage.current-1}
-        />
-        <input placeholder="Limit" onChange={e=>setLimit(e.target.value)}/>
-        <button onClick={changeLimit}>Set Limit</button>
-        <button onClick={logOut} className="btn btn-primary">
+
+        <button
+          onClick={logOut}
+          className="btn btn-primary"
+          style={{ marginTop: 10 }}
+        >
           Log Out
         </button>
       </div>
--- src/components/userHome.js
+++ src/components/userHome.js
@@ -20,3 +20,126 @@ export default function UserHome({ userData }) {
     </div>
   );
 }
+
+
+// import React, {  useEffect, useState } from "react";
+// import { faTrash,faSearch } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// export default function AdminHome({  }) {
+//   //setting state
+//   const [data, setData] = useState([]);
+//   const [searchQuery, setSearchQuery] = useState("");
+
+
+//   useEffect(() => {
+//     getAllUser();
+//   }, [searchQuery]);
+
+//   //fetching all user
+//   const getAllUser = () => {
+//     fetch(`http://localhost:5000/getAllUser?search=${searchQuery}`, {
+//       method: "GET",
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data, "userData");
+//         setData(data.data);
+//       });
+//   };
+//   const handleSearch = (e) => {
+//     setSearchQuery(e.target.value);
+//   };
+
+//   //logout
+//   const logOut = () => {
+//     window.localStorage.clear();
+//     window.location.href = "./sign-in";
+//   };
+
+//   //deleting user
+//   const deleteUser = (id, name) => {
+//     if (window.confirm(`Are you sure you want to delete ${name}`)) {
+//       fetch("http://localhost:5000/deleteUser", {
+//         method: "POST",
+//         crossDomain: true,
+//         headers: {
+//           "Content-Type": "application/json",
+//           Accept: "application/json",
+//           "Access-Control-Allow-Origin": "*",
+//         },
+//         body: JSON.stringify({
+//           userid: id,
+//         }),
+//       })
+//         .then((res) => res.json())
+//         .then((data) => {
+//           alert(data.data);
+//           getAllUser();
+//         });
+//     } else {
+//     }
+//   };
+
+//   return (
+//     <div className="auth-wrapper" style={{ height: "auto", marginTop: 50 }}>
+//       <div className="auth-inner" style={{ width: "fit-content" }}>
+//         <h3>Welcome Admin</h3>
+//         <div style={{ position: "relative", marginBottom: 10 }}>
+//   <FontAwesomeIcon
+//     icon={faSearch}
+//     style={{ position: "absolute", left: 10, top: 13, color: "black" }}
+//   />
+//   <input
+//     type="text"
+//     placeholder="Search..."
+//     value={searchQuery}
+//     onChange={handleSearch}
+//     style={{
+//       padding: "8px 32px 8px 32px",
+//       borderRadius: "4px",
+//       border: "1px solid #ccc",
+//       width: "100%",
+//       boxSizing: "border-box",
+//     }}
+//   />
+//   <span style={{ position: "absolute", right: 10, top: 8, color: "#aaa" }}>
+//     {searchQuery.length>0?`${data.length} Records Found`:`Total Records ${data.length}`} 
+//   </span>
+// </div>
+//         <table style={{ width: 700 }}>
+//           <tr style={{ textAlign: "center" }}>
+//             <th>Name</th>
+//             <th>Email</th>
+//             <th>User Type</th>
+//             <th>Delete</th>
+//           </tr>
+//           {data.map((i) => {
+//             return (
+//               <tr style={{ textAlign: "center" }}>
+//                 <td>{i.fname}</td>
+//                 <td>{i.email}</td>
+//                 <td>{i.userType}</td>
+//                 <td>
+//                   <FontAwesomeIcon
+//                     icon={faTrash}
+//                     onClick={() => deleteUser(i._id, i.fname)}
+//                   />
+//                 </td>
+//               </tr>
+//             );
+//           })}
+//         </table>
+
+//         <button
+//           onClick={logOut}
+//           className="btn btn-primary"
+//           style={{ marginTop: 10 }}
+//         >
+//           Log Out
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
